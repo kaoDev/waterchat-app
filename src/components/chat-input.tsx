02407c7d3aa7462e75648a44423c6866ca4d0959
Kalle Ott <kalle.ott@cap3.de>
2017-06-12 12:37:44 +0200

    code cleanup

---
+++ src/components/chat-input.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react'
+import { PureComponent } from 'react'
+import glamorous from 'glamorous'
+import { grey, lightGrey } from '../colors'
+import { GradienButton } from '../components/generic'
+
+const InputWrapper = glamorous.div({
+  backgroundColor: lightGrey,
+  borderColor: grey,
+  borderBottomStyle: 'solid',
+  borderWidth: '1px',
+  width: '100%',
+  height: '100px',
+  minHeight: '100px',
+  flex: '0',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+})
+
+const TextInput = glamorous.textarea({
+  borderColor: grey,
+  borderBottomStyle: 'solid',
+  borderWidth: '1px',
+  borderRadius: '4px',
+  flexGrow: 1,
+  margin: '20px',
+  outline: 'none',
+  resize: 'none',
+})
+
+type ChatInputState = {
+  messageContent: string,
+}
+
+export type ChatInputProps = {
+  onSubmit: (content: string, channelId: string) => void,
+}
+
+export class ChatInput extends PureComponent<ChatInputProps, ChatInputState> {
+  state = { messageContent: '' }
+
+  onTextInput = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    this.setState({
+      messageContent: (event.target as HTMLTextAreaElement).value,
+    })
+  }
+
+  submit = (event: React.FormEvent<HTMLButtonElement>) => {
+    const content = this.state.messageContent
+    const channelId = 'public'
+    this.props.onSubmit(content, channelId)
+
+    this.setState({
+      messageContent: '',
+    })
+  }
+  render() {
+    return (
+      <InputWrapper key="input">
+        <TextInput
+          onChange={this.onTextInput}
+          value={this.state.messageContent}
+        />
+        <GradienButton onClick={this.submit}>Send</GradienButton>
+      </InputWrapper>
+    )
+  }
+}
--- src/containers/home.tsx
+++ src/containers/home.tsx
@@ -5,40 +5,13 @@ import { AppState } from '../redux/store'
 import { WaterChatAction } from '../events/actions'
 import { SEND_MESSAGE } from '../events/actionIds'
 import { MessageList } from '../components/message-list'
+import { ChatInput } from '../components/chat-input'
 import {
-  GradienButton,
   FlexColumnWrapper,
   FlexColumnCenteredWrapper,
   GradienButtonLink,
 } from '../components/generic'
 import { RouteComponentProps } from 'react-router'
-import glamorous from 'glamorous'
-import { grey, lightGrey } from '../colors'
-
-const InputWrapper = glamorous.div({
-  backgroundColor: lightGrey,
-  borderColor: grey,
-  borderBottomStyle: 'solid',
-  borderWidth: '1px',
-  width: '100%',
-  height: '100px',
-  minHeight: '100px',
-  flex: '0',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-})
-
-const TextInput = glamorous.textarea({
-  borderColor: grey,
-  borderBottomStyle: 'solid',
-  borderWidth: '1px',
-  borderRadius: '4px',
-  flexGrow: 1,
-  margin: '20px',
-  outline: 'none',
-  resize: 'none',
-})
 
 function mapDispatch(dispatch: Dispatch<WaterChatAction>) {
   return {
@@ -58,53 +31,25 @@ const mapState: MapStateToProps<AppState, RouteComponentProps<{}>> = (
 ) => state
 
 type DispatchProps = {
-  sendMessage: (content: string, channelId: string) => void
+  sendMessage: (content: string, channelId: string) => void,
 }
 
-type ChatState = {
-  messageContent: string
-}
+type ChatState = {}
 
 class HomeComponent extends PureComponent<
   AppState & DispatchProps & RouteComponentProps<{}>,
   ChatState
 > {
-  state = { messageContent: '' }
-  onTextInput = (event: React.FormEvent<HTMLTextAreaElement>) => {
-    this.setState({
-      messageContent: (event.target as HTMLTextAreaElement).value,
-    })
-  }
-  submit = (event: React.FormEvent<HTMLButtonElement>) => {
-    const content = this.state.messageContent
-    const channelId = 'public'
-    this.props.sendMessage(content, channelId)
-
-    this.setState({
-      messageContent: '',
-    })
-  }
   render() {
-    const { chatSocket, messages, users, self } = this.props
+    const { chatSocket, messages, users, self, sendMessage } = this.props
 
     return chatSocket === null
       ? <FlexColumnCenteredWrapper>
           <GradienButtonLink to="/login">goto login </GradienButtonLink>
         </FlexColumnCenteredWrapper>
       : <FlexColumnWrapper>
-          <MessageList
-            key="messages"
-            messages={messages}
-            users={users}
-            self={self}
-          />
-          <InputWrapper key="input">
-            <TextInput
-              onChange={this.onTextInput}
-              value={this.state.messageContent}
-            />
-            <GradienButton onClick={this.submit}>Send</GradienButton>
-          </InputWrapper>
+          <MessageList messages={messages} users={users} self={self} />
+          <ChatInput onSubmit={sendMessage} />
         </FlexColumnWrapper>
   }
 }
