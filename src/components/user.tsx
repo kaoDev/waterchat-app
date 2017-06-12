2db68695b4deed30d1190a4c24bac9e7be89a034
Kalle Ott <kalle.ott@cap3.de>
2017-06-13 00:09:06 +0200

    fixed user list styling

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -2,13 +2,20 @@ import * as React from 'react'
 import { ProfilePicture } from './profile-picture'
 import glamorous from 'glamorous'
 
-const Wrapper = glamorous.div({
-  display: 'flex',
-  alignItems: 'center',
-  width: '250px',
-  boxSizing: 'border-box',
-  padding: '8px',
-})
+const Wrapper = glamorous.div<{
+  online: boolean
+}>(
+  {
+    display: 'flex',
+    alignItems: 'center',
+    width: '250px',
+    boxSizing: 'border-box',
+    padding: '8px',
+  },
+  ({ online }) => ({
+    opacity: online ? 1 : 0.5,
+  })
+)
 
 const Name = glamorous.div({
   width: '200px',
@@ -18,11 +25,9 @@ const Name = glamorous.div({
   textOverflow: 'ellipsis',
 })
 
-const StatusAvatar = glamorous(ProfilePicture)<{
-  online: boolean
-}>({}, ({ online }) => ({
-  opacity: online ? 1 : 0.5,
-}))
+const StatusAvatar = glamorous(ProfilePicture)({
+  flexShrink: 0,
+})
 
 export const User = ({
   displayName,
@@ -33,7 +38,7 @@ export const User = ({
   online: boolean
   profilePicture: string
 }) =>
-  <Wrapper>
-    <StatusAvatar online={online} diameter={50} url={profilePicture} />
+  <Wrapper online={online}>
+    <StatusAvatar diameter={50} url={profilePicture} />
     <Name>{displayName}</Name>
   </Wrapper>
